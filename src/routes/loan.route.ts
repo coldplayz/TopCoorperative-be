f2461ee237d40d0d8e9c582795d9cad6489f4b57
greenbel <obisann@gmail.com>
2024-06-08 19:19:34 +0000

    add authorization middleware to loan routes

--- src/routes/loan.route.ts
+++ src/routes/loan.route.ts
@@ -15,28 +15,50 @@ import {
 } from "@/src/controllers/loan.controller";
 import { verifyJWT } from "@/src/middlewares/middleware";
 import { AuthenticatedRequest } from "@/types";
+import { Routes, routeAuthzMap } from "@/lib/config";
 
 const loanRouter = Router();
 
 // Get multiple loans
-loanRouter.get('/', verifyJWT, getLoans);
+loanRouter.get(
+  '/',
+  verifyJWT, // authN
+  routeAuthzMap.get(Routes.loan.getLoans), // authZ
+  getLoans
+);
 
 // Get one loan
-loanRouter.get('/:id', verifyJWT, getLoanById);
+loanRouter.get(
+  '/:id',
+  verifyJWT,
+  routeAuthzMap.get(Routes.loan.getLoanById), // authZ
+  getLoanById
+);
 
 // Create new loan
 loanRouter.post(
   '/',
   verifyJWT,
+  routeAuthzMap.get(Routes.loan.createLoan), // authZ
   (req: Request, res: Response, next: NextFunction) => {
     createLoan(req as AuthenticatedRequest, res, next);
   }
 );
 
 // Update a loan
-loanRouter.put('/:id', verifyJWT, editLoanById);
+loanRouter.put(
+  '/:id',
+  verifyJWT,
+  routeAuthzMap.get(Routes.loan.editLoanById), // authZ
+  editLoanById
+);
 
 // Delete a loan
-loanRouter.delete('/:id', verifyJWT, deleteLoanById);
+loanRouter.delete(
+  '/:id',
+  verifyJWT,
+  routeAuthzMap.get(Routes.loan.deleteLoanById), // authZ
+  deleteLoanById
+);
 
 export default loanRouter;
