e4b545a7e7c79381c337a2613818b1093c6ad967
greenbel <obisann@gmail.com>
2024-06-08 19:05:24 +0000

    add authorization middleware to request routes

--- src/routes/request.route.ts
+++ src/routes/request.route.ts
@@ -14,29 +14,51 @@ import {
   deleteRequestById,
 } from "@/src/controllers/request.controller";
 import { verifyJWT } from "@/src/middlewares/middleware";
+import { Routes, routeAuthzMap } from "@/lib/config";
 import { AuthenticatedRequest } from "@/types";
 
 const requestRouter = Router();
 
 // Get multiple requests
-requestRouter.get('/', verifyJWT, getRequests);
+requestRouter.get(
+  '/',
+  verifyJWT, // authN
+  routeAuthzMap.get(Routes.request.getRequests), // authZ
+  getRequests
+);
 
 // Get one request
-requestRouter.get('/:id', verifyJWT, getRequestById);
+requestRouter.get(
+  '/:id',
+  verifyJWT,
+  routeAuthzMap.get(Routes.request.getRequestById), // authZ
+  getRequestById
+);
 
 // Create new request
 requestRouter.post(
   '/',
   verifyJWT,
+  routeAuthzMap.get(Routes.request.createRequest), // authZ
   (req: Request, res: Response, next: NextFunction) => {
     createRequest(req as AuthenticatedRequest, res, next);
   }
 );
 
 // Update a request
-requestRouter.put('/:id', verifyJWT, editRequestById);
+requestRouter.put(
+  '/:id',
+  verifyJWT,
+  routeAuthzMap.get(Routes.request.editRequestById), // authZ
+  editRequestById
+);
 
 // Delete a request
-requestRouter.delete('/:id', verifyJWT, deleteRequestById);
+requestRouter.delete(
+  '/:id',
+  verifyJWT,
+  routeAuthzMap.get(Routes.request.deleteRequestById), // authZ
+  deleteRequestById
+);
 
 export default requestRouter;
